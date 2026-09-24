1bc31a56b9cd81d4012765d60a462a9f9043cc01
Basant <baghelbasant06@gmail.com>
2026-09-24 23:42:02 +0530

    dabase connection

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { log } = require('console');
 const express=require('express')
+const mongoose=require('mongoose')
 const path=require('path')
 const app=new express();
+const User=require("./models/user")
 
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname,'public')));
@@ -10,12 +12,26 @@ app.listen(3000,(req,res)=>{
     console.log("started successfully");
     
 });
+
+//Connection with mongo DB
+mongoose.connect("mongodb://127.0.0.1:27017/college")
+.then(()=>console.log("Mongodb connected"))
+  .catch(err=>console.log("connection error",err))
+
+//routing
 app.get("/register",(req,res)=>{
 res.render("register")
 });
 
-app.post("/register",(req,res)=>{
-    const {fname,lname,uname,email}=req.body
-    console.log(`first name is ${fname}last name is ${lname}user name is ${uname}email  is ${email}`);
-    res.end('registration successfully');
+app.post("/register",async(req,res)=>{
+    try{
+        const {fname,lname,uname,email}=req.body
+        const user=new User({fname,lname,uname,email})
+        await user.save()
+        res.end('registration successfully');
+    }
+    catch(err){
+        res.status(500).send("server error")
+    }
+    
 });
---
+++ models/user.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose')
+const userSchema=new mongoose.Schema({
+    fname:{type:String,required:true},
+    lname:{type:String,required:true},
+    uname:{type:String,required:true,unique:true},
+    email:{type:String,required:true,unique:true},
+})
+
+module.exports=mongoose.model("user",userSchema)
